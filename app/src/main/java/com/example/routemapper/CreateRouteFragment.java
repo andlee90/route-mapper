8a71bbc4571f8899f7fc3075074b992540ea855d
Andrew Smith <andrw.l.smth@gmail.com>
2016-08-26 00:45:13 -0400

    Add listeners for updated fields

--- app/src/main/java/com/example/routemapper/CreateRouteFragment.java
+++ app/src/main/java/com/example/routemapper/CreateRouteFragment.java
@@ -1,8 +1,5 @@
 package com.example.routemapper;
 
-
-import android.graphics.Color;
-import android.graphics.ColorMatrix;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.KeyEvent;
@@ -10,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,14 +15,14 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.thebluealliance.spectrum.SpectrumDialog;
 import com.thebluealliance.spectrum.SpectrumPalette;
-import com.thebluealliance.spectrum.SpectrumPreference;
 
 import static android.view.View.*;
 import static android.widget.TextView.*;
 
-public class CreateRouteFragment extends Fragment implements OnClickListener, OnEditorActionListener
+
+public class CreateRouteFragment extends Fragment implements OnClickListener, OnEditorActionListener,
+        AdapterView.OnItemSelectedListener, SpectrumPalette.OnColorSelectedListener
 {
     private static final String ARG_ROUTE_ID = "ArgRouteId";
 
@@ -40,7 +38,7 @@ public class CreateRouteFragment extends Fragment implements OnClickListener, On
     private SpectrumPalette mColorPalette;
     private Spinner mLocationSpinner;
     private Spinner mGradeSpinner;
-    private EditText mSetterEdit;
+    private Spinner mSetterSpinner;
 
 
     public static CreateRouteFragment newInstance(int id)
@@ -71,21 +69,28 @@ public class CreateRouteFragment extends Fragment implements OnClickListener, On
 
         mColorPalette = (SpectrumPalette) rootView.findViewById(R.id.palette);
         mColorPalette.setColors(getResources().getIntArray(R.array.colors_array));
+        mColorPalette.setOnColorSelectedListener(this);
 
         mLocationSpinner = (Spinner)rootView.findViewById(R.id.spinner_location);
         ArrayAdapter<CharSequence> locationAdapter = ArrayAdapter.createFromResource(getActivity(),
                 R.array.locations_array, android.R.layout.simple_spinner_item);
         locationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mLocationSpinner.setAdapter(locationAdapter);
+        mLocationSpinner.setOnItemSelectedListener(this);
 
         mGradeSpinner = (Spinner)rootView.findViewById(R.id.spinner_grade);
         ArrayAdapter<CharSequence> gradeAdapter = ArrayAdapter.createFromResource(getActivity(),
                 R.array.grades_array, android.R.layout.simple_spinner_item);
         gradeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mGradeSpinner.setAdapter(gradeAdapter);
+        mGradeSpinner.setOnItemSelectedListener(this);
 
-        mSetterEdit = (EditText)rootView.findViewById(R.id.edit_route_setter);
-        mSetterEdit.setOnEditorActionListener(this);
+        mSetterSpinner = (Spinner)rootView.findViewById(R.id.spinner_setter);
+        ArrayAdapter<CharSequence> setterAdapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.setters_array, android.R.layout.simple_spinner_item);
+        setterAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSetterSpinner.setAdapter(setterAdapter);
+        mSetterSpinner.setOnItemSelectedListener(this);
 
         Button addRoute = (Button)rootView.findViewById(R.id.button_add_route);
         addRoute.setOnClickListener(this);
@@ -135,22 +140,49 @@ public class CreateRouteFragment extends Fragment implements OnClickListener, On
         switch(v.getId())
         {
             case R.id.edit_route_name:
-                if(actionId == EditorInfo.IME_ACTION_DONE){
+                if (actionId == EditorInfo.IME_ACTION_DONE)
+                {
                     mName = mNameEdit.getText().toString();
                 }
                 break;
 
             case R.id.edit_route_date:
-                if(actionId == EditorInfo.IME_ACTION_DONE){
+                if (actionId == EditorInfo.IME_ACTION_DONE)
+                {
                     mDate = mDateEdit.getText().toString();
-                }
-                break;
-            case R.id.edit_route_setter:
-                if(actionId == EditorInfo.IME_ACTION_DONE){
-                    mSetter = mSetterEdit.getText().toString();
                 }
                 break;
         }
         return false;
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+    {
+        switch (view.getId())
+        {
+            case R.id.spinner_grade:
+                mGrade = parent.getItemAtPosition(position).toString();
+                break;
+            case R.id.spinner_location:
+                mLocation = parent.getItemAtPosition(position).toString();
+                break;
+            case R.id.spinner_setter:
+                mSetter = parent.getItemAtPosition(position).toString();
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent)
+    {
+        // Do nothing. Yet.
+    }
+
+    @Override
+    public void onColorSelected(int color)
+    {
+        mColor = color;
+    }
 }
+
